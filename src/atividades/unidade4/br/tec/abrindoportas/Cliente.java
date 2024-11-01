116c9384bf6bd500ac6583fc2820228664f99855
Edir P Santos <fennder@gmail.com>
2024-11-01 18:46:24 -0300

    change Clinte and Curso

--- src/atividades/unidade4/br/tec/abrindoportas/Cliente.java
+++ src/atividades/unidade4/br/tec/abrindoportas/Cliente.java
@@ -55,51 +55,48 @@ public class Cliente {
 	
 	
 	/*	Set<Curso> lcursos;
-	 * public Set<Curso> getLcursos() {
-//		return lcursos;
-//	}
-//
-//
-//	public void setLcursos(Set<Curso> lcursos) {
-//		this.lcursos = lcursos;
-//	}
-//
-//
-//	public static void main(String[] args) {
-//		
-//		Curso java8 = new Curso("Java8");
-//		Curso oracle12c = new Curso("oracle12c");
-//		Curso infraEs = new Curso("infraEs");
-//		
-//		Set<Curso> cursosA = new HashSet<>();
-//		Set<Curso> cursosB = new HashSet<>();
-//
-//		Cliente A = new Cliente();
-//		A.setLcursos(cursosA);
-//		Cliente B = new Cliente();
-//		B.setLcursos(cursosB);
-//
-//		cursosA.add(java8);
-//		cursosA.add(oracle12c);
-//		cursosB.add(java8);
-//		cursosB.add(oracle12c);
-//		cursosB.add(infraEs);
-//
-//		System.out.println(cursosA.toString());
-//		System.out.println(cursosB.toString());
-//		
-//		// interseção
-//		cursosA.retainAll(cursosB);
-//		System.out.println("A interseção de A e B é " + cursosA + "1º Teste");
-//		cursosA.removeAll(cursosB);
-//		System.out.println("Cursos que A faz menos B " + cursosA + "2º Teste");
-//		// B
-//		// interseção
-//		cursosB.retainAll(cursosA);
-//		System.out.println("A interseção de A e B é " + cursosB + "3º Teste");
-//		cursosB.removeAll(cursosA);
-		System.out.println("Cursos que B faz menos A " + cursosB + "4º Teste");
+	public Set<Curso> getLcursos() {
+		return lcursos;
+	}
+
+	public void setLcursos(Set<Curso> lcursos) {
+		this.lcursos = lcursos;
+	}
+
+	public static void main(String[] args) {
 		
+		Curso java8 = new Curso("Java8");
+		Curso oracle12c = new Curso("oracle12c");
+		Curso infraEs = new Curso("infraEs");
+		
+		Set<Curso> cursosA = new HashSet<>();
+		Set<Curso> cursosB = new HashSet<>();
+
+		Cliente A = new Cliente();
+		A.setLcursos(cursosA);
+		Cliente B = new Cliente();
+		B.setLcursos(cursosB);
+
+		cursosA.add(java8);
+		cursosA.add(oracle12c);
+		cursosB.add(java8);
+		cursosB.add(oracle12c);
+		cursosB.add(infraEs);
+
+		System.out.println(cursosA.toString());
+		System.out.println(cursosB.toString());
+		
+		// interseção
+		cursosA.retainAll(cursosB);
+		System.out.println("A interseção de A e B é " + cursosA + "1º Teste");
+		cursosA.removeAll(cursosB);
+		System.out.println("Cursos que A faz menos B " + cursosA + "2º Teste");
+		// B
+		// interseção
+		cursosB.retainAll(cursosA);
+		System.out.println("A interseção de A e B é " + cursosB + "3º Teste");
+		cursosB.removeAll(cursosA);
+		System.out.println("Cursos que B faz menos A " + cursosB + "4º Teste");
 		
 	}*/
 
