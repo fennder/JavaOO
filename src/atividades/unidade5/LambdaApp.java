dafcfb65819475d2e87c8f9541f5a83d3d0628e2
Edir P Santos <fennder@gmail.com>
2024-11-02 12:15:49 -0300

    Expressão Lambda

--- src/atividades/unidade5/LambdaApp.java
+++ src/atividades/unidade5/LambdaApp.java
@@ -1,27 +1,34 @@
 package atividades.unidade5;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class LambdaApp {
 
+	int somatoria;
 	public static void main(String[] args) {
 		
-		List<Integer> integers = Arrays.asList(1,2,3,4,5,6,7,8);
-		
+		List<Integer> integers = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+		LambdaApp app = new LambdaApp();
+		int numero = 10;
 		/*for (Integer integer : integers) {
 			integer = integer + 10;
 			System.out.println(integer);
 
 		}*/
 		
-		integers.forEach(x->
+		integers.forEach((Integer x)->System.out.println(x));
+		
+		integers.forEach((Integer x)->
 		{
-			x = x + 10;
+			int y = x/2;
+			System.out.println(y);
+			x = x + numero;
+			app.somatoria = app.somatoria + x;
 			System.out.println(x);
 			
 		});
+		System.out.println(app.somatoria);
 
 	}
 
