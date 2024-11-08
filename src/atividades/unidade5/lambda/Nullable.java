b22ac330c40d12f1b43059addcbf7e709c24b15c
Edir P Santos <fennder@gmail.com>
2024-11-08 02:20:46 -0300

    complementação

--- src/atividades/unidade5/lambda/Nullable.java
+++ src/atividades/unidade5/lambda/Nullable.java
@@ -1,11 +1,7 @@
 package atividades.unidade5.lambda;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-@Retention
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 
