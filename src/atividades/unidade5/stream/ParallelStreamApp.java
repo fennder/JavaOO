f1db4b81893d3ed8f8aa74af2fd259e81c71c9b7
Edir P Santos <fennder@gmail.com>
2024-11-14 10:16:05 -0300

    ParallelStream

---
+++ src/atividades/unidade5/stream/ParallelStreamApp.java
@@ -0,0 +1,44 @@
+package atividades.unidade5.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStreamApp {
+
+	public static void main(String[] args) {
+		
+		colecaoStream();
+		colecaoParallelStream();
+		
+	}
+
+	private static void colecaoParallelStream() {
+		
+		List<String> valores = new ArrayList<>();
+		int max =100;
+		
+		for (int i = 0; i < max; i++) {
+			valores.add("=>"+i);
+		}
+		long t0 = System.currentTimeMillis();
+		valores.parallelStream().sorted().forEach(System.out::println);
+		long t1 = System.currentTimeMillis();
+		System.out.println("Tempo consumido " + (t1-t0));
+	}
+
+	private static void colecaoStream() {
+		
+		List<String> valores = new ArrayList<>();
+		int max =1_000_000;
+		
+		for (int i = 0; i < max; i++) {
+			valores.add("=>"+i);
+		}
+		long t0 = System.currentTimeMillis();
+		valores.stream().sorted().forEach(System.out::println);
+		long t1 = System.currentTimeMillis();
+		System.out.println("Tempo consumido " + (t1-t0));
+		
+	}
+
+}
