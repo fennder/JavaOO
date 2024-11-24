f95ea606ecb9436ce31d70c4b02f7ad20d2ceff6
Edir P Santos <fennder@gmail.com>
2024-11-24 17:43:55 -0300

    Insert Component

--- src/atividades/unidade6/ContentApp.java
+++ src/atividades/unidade6/ContentApp.java
@@ -1,10 +1,14 @@
 package atividades.unidade6;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ContentApp {
 
@@ -13,8 +17,14 @@ public class ContentApp {
 		JFrame frame = new JFrame("Primeira Janela da Aplicação");
 		frame.setSize(800,600);
 		Container container = frame.getContentPane();
-		container.setLayout(new GridLayout(2,1));
-		container.add(new JLabel("Isto é um rótulo!"));
+		JTextField jtxtfield = new JTextField("Digite Aqui: ");
+		container.add(jtxtfield,BorderLayout.NORTH);
+		JPasswordField jpfield = new JPasswordField();
+		container.add(jpfield,BorderLayout.SOUTH);
+		JTextArea jtarea = new JTextArea("Digite o texto aqui:");
+		container.add(jtarea,BorderLayout.CENTER);
+//		container.setLayout(new GridLayout(2,1));
+//		container.add(new JLabel("Isto é um rótulo!"));
 		frame.setVisible(true);
 
 	}
