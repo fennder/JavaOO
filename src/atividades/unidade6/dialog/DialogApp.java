1ea7bad3bdeb4356203acbaf899dc9a2ce9fac1b
Edir P Santos <fennder@gmail.com>
2024-12-07 14:41:03 -0300

    Estruturação de código

--- src/atividades/unidade6/dialog/DialogApp.java
+++ src/atividades/unidade6/dialog/DialogApp.java
@@ -6,17 +6,15 @@ public class DialogApp {
 
 	public static void main(String[] args) {
 		int resposta = JOptionPane.showConfirmDialog(null, "Documento modfiado. Salvar as alterações?");
+		
 		var resposta1 = JOptionPane.showInputDialog(null, "Digite seu nome", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
-		JOptionPane.showMessageDialog(null, "VAlores passados");
+		
+		JOptionPane.showMessageDialog(null, "Valores passados");
+		
 		String[] opcoes = {"Voltar", "Repetir", "Avançar"};
-		resposta = JOptionPane.showOptionDialog(null, 
-				"Você fez 1000 pontos nesta fase.", 
-				"Jogo", 
-				0, 
-				JOptionPane.QUESTION_MESSAGE, 
-				null,
-				opcoes,
-				opcoes[2]);
+		
+		resposta = JOptionPane.showOptionDialog(null, "Você fez 1000 pontos nesta fase.", "Jogo", 0, 
+				JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[2]);
 	}
 
 }
